36420158ab21ac4fea0b305847532f29f8406a56
Josh Guilfoyle <devjasta@fb.com>
2015-02-22 12:09:02 -0800

    Add favicon support to chrome://inspect
    Note that the icon is currently not showing up due to some problem with
    the Chrome client.  I believe they may have regressed the favicon
    feature in the latest release.

--- stetho/src/main/java/com/facebook/stetho/inspector/ChromeDiscoveryHandler.java
+++ stetho/src/main/java/com/facebook/stetho/inspector/ChromeDiscoveryHandler.java
@@ -1,30 +1,33 @@
 package com.facebook.stetho.inspector;
 
-import javax.annotation.Nullable;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
-
 import com.facebook.stetho.common.Utf8Charset;
 import com.facebook.stetho.server.LocalSocketHttpServer;
 import com.facebook.stetho.server.SecureHttpRequestHandler;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpRequestHandlerRegistry;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import javax.annotation.Nullable;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Provides sufficient responses to convince Chrome's {@code chrome://inspect/devices} that we're
@@ -38,6 +41,7 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
   private static final String PATH_PAGE_LIST = "/json";
   private static final String PATH_VERSION = "/json/version";
   private static final String PATH_ACTIVATE = "/json/activate/" + PAGE_ID;
+  private static final String PATH_FAVICON = "/favicon.png";
 
   /**
    * Latest version of the WebKit Inspector UI that we've tested again (ideally).
@@ -57,6 +61,7 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
 
   @Nullable private StringEntity mVersionResponse;
   @Nullable private StringEntity mPageListResponse;
+  @Nullable private ByteArrayEntity mFaviconResponse;
 
   public ChromeDiscoveryHandler(Context context, String inspectorPath) {
     super(context);
@@ -68,6 +73,7 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
     registry.register(PATH_PAGE_LIST, this);
     registry.register(PATH_VERSION, this);
     registry.register(PATH_ACTIVATE + "*", this);
+    registry.register(PATH_FAVICON, this);
   }
 
   @Override
@@ -85,6 +91,8 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
         handlePageList(response);
       } else if (PATH_ACTIVATE.equals(path)) {
         handleActivate(response);
+      } else if (PATH_FAVICON.equals(path)) {
+        handleFavicon(response);
       } else {
         response.setStatusCode(HttpStatus.SC_NOT_IMPLEMENTED);
         response.setReasonPhrase("Not Implemented");
@@ -120,6 +128,7 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
       page.put("title", getAppLabel() + " (powered by Stetho)");
       page.put("id", PAGE_ID);
       page.put("description", "");
+      page.put("faviconUrl", PATH_FAVICON);
 
       page.put("webSocketDebuggerUrl", "ws://" + mInspectorPath);
       Uri chromeFrontendUrl = new Uri.Builder()
@@ -141,6 +150,43 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
   private void handleActivate(HttpResponse response) throws UnsupportedEncodingException {
     // Arbitrary response seem acceptable :)
     setSuccessfulResponse(response, createStringEntity("text/plain", "Target activation ignored"));
+  }
+
+  private void handleFavicon(HttpResponse response) {
+    if (mFaviconResponse == null) {
+      Bitmap iconAsBitmap = getBitmapFromDrawable(getAppIcon());
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      iconAsBitmap.compress(
+          Bitmap.CompressFormat.PNG,
+          90 /* quality */,
+          out);
+      ByteArrayEntity entity = new ByteArrayEntity(out.toByteArray());
+      entity.setContentEncoding("image/png");
+      mFaviconResponse = entity;
+    }
+    setSuccessfulResponse(response, mFaviconResponse);
+  }
+
+  private static Bitmap getBitmapFromDrawable(Drawable d) {
+    if (d instanceof BitmapDrawable) {
+      return ((BitmapDrawable)d).getBitmap();
+    } else {
+      int width;
+      int height;
+      if (d.getIntrinsicWidth() > 0 && d.getIntrinsicHeight() > 0) {
+        width = d.getIntrinsicWidth();
+        height = d.getIntrinsicHeight();
+      } else {
+        throw new IllegalArgumentException("Must use drawables with intrinsic dimenions");
+      }
+      Bitmap bitmap = Bitmap.createBitmap(
+          width,
+          height,
+          Bitmap.Config.ARGB_4444);
+      Canvas c = new Canvas(bitmap);
+      d.draw(c);
+      return bitmap;
+    }
   }
 
   private static StringEntity createStringEntity(String contentType, String responseJson)
@@ -175,5 +221,10 @@ public class ChromeDiscoveryHandler extends SecureHttpRequestHandler {
   private CharSequence getAppLabel() {
     PackageManager pm = mContext.getPackageManager();
     return pm.getApplicationLabel(mContext.getApplicationInfo());
+  }
+
+  private Drawable getAppIcon() {
+    PackageManager pm = mContext.getPackageManager();
+    return pm.getApplicationIcon(mContext.getApplicationInfo());
   }
 }
